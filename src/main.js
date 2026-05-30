e596d74166b788e2fcc890c9e15ad10a78b55718
berto <rdmccann@gmail.com>
2026-05-30 13:27:27 -0600

    better feature appending/data joining

--- src/main.js
+++ src/main.js
@@ -4,34 +4,50 @@ import links from './link-profiles/default.json'
 
 // const svg = d3.select("svg");
 
-const width = 600;
+const width = 1200;
 const height = 400;
-const margin = { top: 0, right: 0, bottom: 0, left: 0 };
+const margin = { top: 10, right: 10, bottom: 20, left: 30 };
 
-const x = d3.scaleLinear()
-    .range([margin.left, width - margin.right])
-    .domain([0, 600]);
 
-const y = d3.scaleLinear()
+const xmax = links.reduce((max = 0, item) => {
+            const dxmax = item.data.reduce((m, d) => d.x > m ? d.x : m, 0);
+            return dxmax > max ? dxmax : max
+    },  0);
+
+const ymax = links.reduce((max = 0, item) => {
+        const dymax = item.data.reduce((m, d) => d.y > m ? d.y : m, 0);
+        return dymax > max ? dymax : max
+    },  0);
+
+console.log(xmax, ymax)
+
+const xScale = d3.scaleLinear()
+    .range([margin.left, width - margin.right]) // pixels
+    // .range(d3.extent(links, d => d.data[0].x))
+    .domain([0, xmax*1.1]).nice() // real world units
+    // .domain(d3.extent(links, (d => d3.extent(d.data, (d1 => d1.x))))).nice()
+
+const yScale = d3.scaleLinear()
     .range([height - margin.bottom, margin.top])
-    .domain([0, 400]);
+    .domain([0, ymax]).nice()
+    // .domain(d3.extent(links, (d => d3.extent(d.data, (d1 => d1.y))))).nice()
 
 const svg = d3.create("svg")
     .attr("width", width)
     .attr("height", height)
 
 // Add the x-axis.
-// svg.append("g")
-//     .attr("transform", `translate(0,${height - margin.bottom})`)
-//     // .style("stroke", "#000")
-//     .classed("axis", true)
-//     .call(d3.axisBottom(x));
+svg.append("g")
+    .attr("transform", `translate(0,${height - margin.bottom})`)
+    // .style("stroke", "#000")
+    .classed("axis", true)
+    .call(d3.axisBottom(xScale));
 
 // Add the y-axis.
-// svg.append("g")
-//     .attr("transform", `translate(${margin.left},0)`)
-//     .classed("axis", true)
-//     .call(d3.axisLeft(y));
+svg.append("g")
+    .attr("transform", `translate(${margin.left},0)`)
+    .classed("axis", true)
+    .call(d3.axisLeft(yScale));
 
 // line constructor
 const line = d3.line()
@@ -58,41 +74,42 @@ links.map(link => {
     intersectLinks2D(link)
 })
 
-links.forEach((link, i) => {
-    svg.append("path")
-        // .datum(link.data)
-        .data([link.data])
-        .attr("d", line)
-        .attr("id", link.id)
-        .classed("linkLine", true)
-        .classed(link.linkSet, true)
-        .classed(link.position, true)
-        .classed("extendedLink", link.ext)
 
-    if(!link.ext) {
-        svg.append("circle")
-            .attr("cx", link.data[1].x)
-            .attr("cy", link.data[1].y)
-            .attr("r", 5)
-            .attr("id", i)
-            .classed("endPoint", true)
-            .classed(link.linkSet, true)
-            .classed(link.position, true)
-        .style("cursor", "pointer")
-        .call(d3.drag().on("drag", draggy)
-            .on("start", event => {
-                d3.select(event.sourceEvent.target).classed("grabbing", true);
-            })
-            .on("drag", draggy)
-            .on("end", event => {
-                d3.select(event.sourceEvent.target).classed("grabbing", false);
-            })
-        )
-    }
-})
+// add control points
+svg.selectAll(".controlPoint")
+    .data(links)
+    .join("circle")
+    .attr("id", l => l.id)
+    // .attr("cx", d => xScale(d.data[1].x))
+    // .attr("cy", d => yScale(d.data[1].y))
+    .attr("cx", d => d.data[1].x)
+    .attr("cy", d => d.data[1].y)
+    .attr("r", 5)
+    .classed("controlPoint", true)
+    .call(d3.drag().on("drag", draggy)
+        .on("start", event => {
+            d3.select(event.sourceEvent.target).classed("grabbing", true);
+        })
+        .on("drag", draggy)
+        .on("end", event => {
+            d3.select(event.sourceEvent.target).classed("grabbing", false);
+        })
+    )
+
+svg.selectAll(".linkLine")
+    .data(links, l => l.id)
+    .join("path")
+    .attr("d", l => line(l.data))
+    .attr("id", link => link.id)
+    .classed("linkLine", true)
+    .classed(link => link.linkSet, true)
+    .classed(link => link.position, true)
+    .classed("extendedLink", link => link.ext)
+
 
 // add the svg to the container
 d3.select("#container").append(() => svg.node());
+
 
 // 4. Drag event function
 function draggy(event) {
@@ -102,7 +119,7 @@ function draggy(event) {
         .attr("cy", event.y);
 
     // Update link-profiles coordinates to match current mouse/touch position
-    const id = Number(this.id.replace("endPoint", ""));
+    const id = Number(this.id.replace("controlPoint", ""));
     links[id].data[1].x = event.x;
     links[id].data[1].y = event.y;
 
@@ -143,9 +160,9 @@ function extendLink(inLink) {
     const len = Math.sqrt(dx**2 + dy**2);
 
     // Define how much longer you want the line to be
-    const ext = 500;
+    const ext = 1200;
 
-    // Extrapolate the new endpoint (x3, y3)
+    // Extrapolate the new controlPoint (x3, y3)
     const x3 = x2 + (dx / len) * ext;
     const y3 = y2 + (dy / len) * ext;
 
@@ -154,7 +171,7 @@ function extendLink(inLink) {
         {x: x3, y: y3}
     ]
 
-    console.log("extended Link", inLink, outDatum);
+    // console.log("extended Link", inLink, outDatum);
 
     return outDatum;
 }
@@ -166,7 +183,11 @@ function intersectLinks2D(linkA) {
     const ay2 = linkA.data[1].y;
 
     // linkB will be the other vertical position extended line
-    const linkB = links.find(l => (l.linkSet === linkA.linkSet && l.position !== linkA.position && l.position !== "panhard" && l.ext === true));
+    const linkB = links.find(l => (
+        l.linkSet === linkA.linkSet
+        && l.position !== linkA.position
+        && l.position !== "panhard" && l.ext === true
+    ));
 
     const bx1 = linkB.data[0].x;
     const by1 = linkB.data[0].y;
@@ -185,7 +206,7 @@ function intersectLinks2D(linkA) {
             const intersectionX = Number(ax1 + (ax2 - ax1) * t);
             const intersectionY = Number(ay1 + (ay2 - ay1) * t);
 
-            // set the link extensions =
+            // trim the link extensions
             links.filter(l => (l.linkSet === linkA.linkSet && l.ext === true))
                 .map((l) => {
                     l.data[1].x = intersectionX;
